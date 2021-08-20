0b89d961b8a6a4ef1151b54570732f4486c6a2cb
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-08-20 13:01:42 +0700

    Update post.routes.ts

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -5,7 +5,6 @@ export const router: Router = Router();
 
 router.get('/', async (req, res) => {
     try {
-      //console.log(req);
       const posts = await Posts.find();
       console.log(posts);
       res.json(posts);
@@ -13,3 +12,7 @@ router.get('/', async (req, res) => {
       res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
     }
   })
+
+router.post('/', async (req, res) => {
+
+})
