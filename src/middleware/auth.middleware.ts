f18753ff67f07073f3a178b8bdfcd2bd2129470d
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-08-23 13:15:22 +0700

    Update auth.middleware.ts

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,1 +1,20 @@
+import jwt from 'jsonwebtoken'
+import {IConf} from 'interfaces/config'
+import config from 'config/default.json'
 
+config as IConf;
+
+export default authMiddleware = (req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return next()
+  }
+  
+  try {
+    
+  }
+  
+  catch {
+    res.status(401).json({ message: `You has not been auth` })
+  }
+
+}
