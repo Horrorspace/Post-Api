b1070fa5cc42018b6c4875cb4494ae0fb651adfe
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-08-23 21:55:54 +0700

    auth middleware edit

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,16 +1,24 @@
 import jwt from 'jsonwebtoken'
-import {IConf} from 'interfaces/config'
-import config from 'config/default.json'
+import {IConf} from '../interfaces/config'
+import config from '../config/default.json'
 
 config as IConf;
 
-export default authMiddleware = (req, res, next) => {
+export const authMiddleware = (req, res, next) => {
   if (req.method === 'OPTIONS') {
     return next()
   }
   
   try {
-    
+    const token = req.headers.authorization.split(' ')[1];
+ 
+    if(!token) {
+      return res.status(401).json({ message: `You has not been auth` })
+    }
+
+    const decoded = jwt.verify(token, config.jwtSecret);
+    req.user = decoded;
+    next()
   }
   
   catch {
